b145fadfc167f8586093bbd0ae479084a1d8afea
xukai <xukai.ken@bytedance.com>
2019-04-19 15:06:50 +0800

    ADD

--- utils/Utils.java
+++ utils/Utils.java
@@ -5,10 +5,27 @@ public class Utils {
     /**
      * Determine if the request is static or not .
      *
+     *
+     *
+     *
+     *
      * @param uri
      * @return
      */
     public static boolean isStaticRequest(String uri) {
+
+        int uriLength = uri.length();
+        while (uriLength >= 0) {
+            Character character = uri.charAt(uriLength);
+            if (character.equals(".")) {
+
+            }
+
+
+
+            uriLength--;
+        }
+
         return false;
     }
 }
