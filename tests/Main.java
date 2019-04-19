27d6fedddf784160a20b31f4d90e60c741641db1
xukai <xukai.ken@bytedance.com>
2019-04-19 11:22:14 +0800

    ADD

---
+++ tests/Main.java
@@ -0,0 +1,20 @@
+package tests;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        /*
+        Executor executor = new ThreadPoolExecutor();
+
+
+        Thread t = new Thread();
+        executor.execute(t);
+
+
+         */
+    }
+}
